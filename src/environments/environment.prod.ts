b261107766dd96a7d759e8f57a157f7a4d7907eb
Camila Moreno <cam.morenop@duocuc.cl>
2025-07-13 01:51:59 -0400

    Detalles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.149:3000' //'https://jsonplaceholder.typicode.com/'
+  e2e: false,
+  apiUrl: 'http://192.168.1.149:3000' 
 }; 
 
